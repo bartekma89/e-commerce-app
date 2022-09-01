7f226d0838aac7521b176713d9b83f10cfb99db1
Bartłomiej Marzec <bmarzec@sii.pl>
2022-09-01 23:29:49 +0200

    added stripe

--- components/Cart/CartContent.tsx
+++ components/Cart/CartContent.tsx
@@ -1,9 +1,54 @@
+import TrashIcon from "@heroicons/react/outline/TrashIcon";
+import { loadStripe } from "@stripe/stripe-js";
+
 import { useCartState } from "@/context/CartStateContext";
 import { formatNumber } from "@/lib/helpers";
-import TrashIcon from "@heroicons/react/outline/TrashIcon";
+import Stripe from "stripe";
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+);
 
 const CartContent = () => {
   const cartState = useCartState();
+
+  const pay = async () => {
+    const stripe = await stripePromise;
+
+    if (!stripe) {
+      throw new Error("Something went wrong");
+    }
+
+    const res = await fetch("/api/checkout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(
+        cartState.items.map((cartItem) => {
+          return {
+            slug: cartItem.id,
+            count: cartItem.count,
+          };
+          // return {
+          //   price_data: {
+          //     currency: "PLN",
+          //     unit_amount: Math.round(cartItem.price * 100),
+          //     product_data: {
+          //       name: cartItem.title,
+          //     },
+          //   },
+          //   quantity: cartItem.count,
+          // };
+        })
+      ),
+    });
+
+    const { session }: { session: Stripe.Response<Stripe.Checkout.Session> } =
+      await res.json();
+
+    await stripe.redirectToCheckout({ sessionId: session.id });
+  };
 
   return (
     <div className="col-span-2">
@@ -33,6 +78,16 @@ const CartContent = () => {
           );
         })}
       </ul>
+      {cartState.items.length === 0 ? (
+        <div>Cart is empty</div>
+      ) : (
+        <button
+          onClick={pay}
+          className="rounded-lg bg-black text-sm p-2.5 text-white w-full flex justify-center"
+        >
+          Submit your order
+        </button>
+      )}
     </div>
   );
 };
